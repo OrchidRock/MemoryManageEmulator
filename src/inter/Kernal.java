7fbe2070b0d228b30491b8b5faf6c37f829b27fa
Rock <rocksevencolor@gmail.com>
2016-07-05 18:22:23 +0000

    Add load Page table size optimize policy

--- src/inter/Kernal.java
+++ src/inter/Kernal.java
@@ -1,5 +1,6 @@
 package inter;
 
+import java.awt.MultipleGradientPaint.ColorSpaceType;
 import java.util.Properties;
 
 import jdk.internal.dynalink.beans.StaticClass;
@@ -10,18 +11,23 @@ public class Kernal {
 	
 	public final static int
 		RANDOM=0,FIFO=1,LRU=2;
-	
+	public final static int 
+		Inverted=5,Traditional=6;
+	    
 	private static Kernal kernal;
 	private Kernal(){}
 	
 	
 	public static int LABN=-1;
 	public static int PABN=-1;
+	public static int PtSizeOptimizePolicy=-1; //Page Table SizeOptimizePolicy
+	
+	public static int PageTableMaxIndex=-1;
+	
 	
 	private int replacePolicy=-1;
 	//allcote pcb number
 	private int PidMaxIndex=0;
-	public  int PageTableMaxIndex=-1;
 	public static Kernal createNewInstance(){
 		if(kernal==null)
 			kernal=new Kernal();
@@ -29,6 +35,10 @@ public class Kernal {
 		Properties properties=ConfLoader.getPropertiesByConfType(ConfType.Kernal);
 		LABN=Integer.valueOf(properties.getProperty("LABN"));
 		PABN=Integer.valueOf(properties.getProperty("PABN"));
+		
+		Properties properties_pt=ConfLoader.getPropertiesByConfType(ConfType.PageTable);
+		PtSizeOptimizePolicy=Integer.valueOf(properties_pt.getProperty("SizeOptimizePolicy"));
+		
 		kernal.replacePolicy=kernal.getReplacePolicyTag(properties.getProperty("ReplacePolicy"));
 		return kernal;
 	}
@@ -39,6 +49,11 @@ public class Kernal {
 		PidMaxIndex++;
 		PageTableMaxIndex++;
 		PCB pcb=new PCB(PidMaxIndex,PageTableMaxIndex);
+		
+		
+		
+		
+		
 		return pcb;
 	}
 	public static int getReplacePolicyTag(String policyname){
